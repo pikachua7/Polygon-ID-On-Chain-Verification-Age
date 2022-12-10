7215f7f8b316bcf4008ee9434e664ea8ec045556
pikachua7 <atharvapaliwal7@gmail.com>
2022-12-11 02:33:32 +0530

    configure hardhat file

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,8 +5,8 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/477a4r1CxgMaMcbhdfTIYvRiqyfxeT9H`,
-      accounts: [`0x6b204426c792645cde36b9cb39dae4dfd890da97b115aba14aab3c6bba675d70`],
+      url: `MUMBAI_RPC_URL`,
+      accounts: [`YOUR_PRIVATE_KEY`],
     }
   }
 };
